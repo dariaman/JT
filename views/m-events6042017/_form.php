eeb2651f3492582bbe25ee73749587dfe7fd337a
wawan <aditiyakurniawan30@gmail.com>
2017-04-06 15:57:17 +0700

    fixing sample project and compile gradle added

--- views/m-events6042017/_form.php
+++ views/m-events6042017/_form.php
@@ -1,8 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
-use kartik\date\DatePicker;
+use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\MEvents */
@@ -11,38 +10,23 @@ use kartik\date\DatePicker;
 
 <div class="mevents-form">
 
-   <?php $form = ActiveForm::begin([
-    'id'=>$model->formName()
-   'layout' => 'horizontal'
+    <?php $form = ActiveForm::begin(); ?>
 
-   ]); ?>
-
+    <?= $form->field($model, 'eventId')->textInput() ?>
 
     <?= $form->field($model, 'eventJudul')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'eventDeskripsi')->textarea(['rows' => 6]) ?>
 
-
-    <?= $form->field($model, 'eventTgl')->widget(DatePicker::classname(), [
-        'options' => ['placeholder' => 'Enter  date ...'],
-        'pluginOptions' => [
-            'autoclose'=>true
-        ]
-    ]) ?>
+    <?= $form->field($model, 'eventTgl')->textInput() ?>
 
     <?= $form->field($model, 'eventGambarUrl')->textInput(['maxlength' => true]) ?>
 
-      <?php
-    if(!$model->isNewRecord){
-    ?>
+    <?= $form->field($model, 'eventDibuatTgl')->textInput() ?>
 
+    <?= $form->field($model, 'eventDibuatOleh')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'eventStatus')->textInput(['maxlength' => true]) ?>
-
-    <?php
-}
-
-    ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
